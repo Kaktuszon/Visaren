bb63481ff5afbf110d09c71f02333beea7373ae8
Kaktuszon <kaktus@kaktuszon.org>
2026-09-24 18:54:37 +0200

    Add spells for druid

---
+++ Druid.lua
@@ -0,0 +1,11 @@
+spellNamesDruid = {
+    "Berserk",
+    "Savage Defense",
+    "Nature's Vigil",
+}
+
+spellIconsDruid = {
+    GetSpellTexture(50334),
+    GetSpellTexture(62606),
+    GetSpellTexture(124974),
+}
