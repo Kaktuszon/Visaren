0eec6d8252a962f5dbb1ed9b386cb01766ccf28e
Kaktuszon <kaktus@kaktuszon.org>
2026-09-24 19:02:19 +0200

    Add more spells to druid

--- Druid.lua
+++ Druid.lua
@@ -1,11 +1,19 @@
 spellNamesDruid = {
     "Berserk",
+    "Survival Insticts",
+    "Barkskin",
     "Savage Defense",
+    "Might of Ursoc",
+    "Dancing Steel",
     "Nature's Vigil",
 }
 
 spellIconsDruid = {
     GetSpellTexture(50334),
+    GetSpellTexture(61336),
+    GetSpellTexture(22812),
     GetSpellTexture(62606),
+    GetSpellTexture(105737),
+    GetSpellTexture(118334),
     GetSpellTexture(124974),
 }
