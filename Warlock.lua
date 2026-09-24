c1ed25731a79b195d5bfc5d51d198686b05a16de
Kaktuszon <kaktus@kaktuszon.org>
2026-09-24 18:54:25 +0200

    Move Warlock spells to seperate file

---
+++ Warlock.lua
@@ -0,0 +1,15 @@
+spellNamesWarlock = {
+    "Expanded Mind",
+    "Toxic Power",
+    "Jade Spirit",
+    "Dark Soul: Instability",
+    "Burning Rush",
+}
+
+spellIconsWarlock = {
+    GetSpellTexture(146046),
+    GetSpellTexture(148906),
+    GetSpellTexture(104993),
+    GetSpellTexture(77801),
+    GetSpellTexture(111400),
+}
